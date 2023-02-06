9892ab5f5ead96729cb86e1c50de39a5fe2d3187
MARIUS-SAMO <samemarius@gmail.com>
2023-02-06 09:24:56 +0100

    new best

--- submissions/lgb/estimator.py
+++ submissions/lgb/estimator.py
@@ -3,10 +3,18 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.base import BaseEstimator
 
+def create_feature(data):
+    data = data.copy()
+    data["hour"] = data.index.hour
+    data["dayofweek"] = data.index.dayofweek
+    data["year"] = data.index.year
+    data["dayofyear"] = data.index.dayofyear
+    return data
+
 
 
 def compute_rolling_std(X_df, feature, sub_feature, 
-        time_window , l_alpha, types = ["rolling", "emv"], center=False):
+        time_window , l_alpha, types = ["rolling", "emv"], center=True):
     """
     For a given dataframe, compute the standard deviation over
     a defined period of time (time_window) of a defined feature
@@ -28,7 +36,6 @@ def compute_rolling_std(X_df, feature, sub_feature,
     for type in types :
         if type == "rolling":
             for time in time_window :
-                    print(type + "here")
                     #data = X_df[feature].rolling(time, center=center).agg(("sum",  "min", "max","kurt", "skew", "median", lambda x: x.quantile(0.75)))
                     data = X_df[feature].rolling(time, center=center).agg(("mean", "std", "median","min", "max","kurt", "skew", "sum"))
                     name = [f"_{str(time)}_".join(a) for a in data.columns.to_list()]
@@ -38,7 +45,6 @@ def compute_rolling_std(X_df, feature, sub_feature,
         if type == "emv":
 
             for alpha in l_alpha:
-                    print(type + "here")
                     data = X_df[feature].ewm(alpha=alpha).agg({"mean", "std"})
                     name = [f"_{str(alpha)}_".join(a) for a in data.columns.to_list()]
                     X_df[name] = data.values
@@ -48,7 +54,7 @@ def compute_rolling_std(X_df, feature, sub_feature,
 
 
 class FeatureExtractor(BaseEstimator):
-    def __init__(self, feature , window= ["2h", "24h"], alpha = [0.2, 0.5, 0.7]) -> None:
+    def __init__(self, feature , window= ["10h","30h", "24h"], alpha = [0.2, 0.5, 0.7]) -> None:
         super().__init__()
         self.window = window
         self.feature = feature
@@ -57,8 +63,8 @@ class FeatureExtractor(BaseEstimator):
         return self
 
     def transform(self, X):
-        return compute_rolling_std(X_df=X, feature=self.feature, sub_feature=self.feature, time_window=self.window,
-        l_alpha=self.alpha)
+        return create_feature( compute_rolling_std(X_df=X, feature=self.feature, sub_feature=self.feature, time_window=self.window,
+        l_alpha=self.alpha))
 
 
 def get_estimator():
@@ -66,7 +72,7 @@ def get_estimator():
     feature_extractor = FeatureExtractor(feature=['B', 'Range F 1', 'Range F 13', 'Range F 9', 'Vth', 'Vx', 'Vy',
     'Beta', 'Pdyn', 'RmsBob'])
 
-    params={'boosting_type': 'gbdt', 'learning_rate': 0.047403318065537894, 'reg_alpha': 61.819153561585296, 'reg_lambda': 1.637753769029222}
+    params={'learning_rate': 0.04944582934277474, 'reg_alpha': 96.4338109627943, 'reg_lambda': 41.771440699357434}
 
     classifier = LGBMClassifier(**params, n_jobs=-1)
     pipe = make_pipeline(feature_extractor, classifier)
